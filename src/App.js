90b50d4d8f89fc0322c627371de48a70884bdbad
satou <stuysyk1027@icloud.com>
2021-04-01 07:46:46 +0900

    react-router-practice2

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+
+function App() {
+  return (
+  <div className='App'>
+    <Router>
+    <ul>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/about'>About</Link></li>
+    </ul>
+      <Route path='/' exact component={Home} />
+      <Route path='/about/:id' exact component={About} />
+    </Router>
+   </div>
+  );
+}
+
+export default App;
